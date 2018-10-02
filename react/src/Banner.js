0e77639a21baa31512742c945fa97cf290869d2c
Jimmy Messmer <james.messmer@vumc.org>
2018-10-02 11:58:18 -0500

    HANDELP-26 Added overlay to particles and changed particles to move to top right

--- react/src/Banner.js
+++ react/src/Banner.js
@@ -17,7 +17,7 @@ import Particles from 'react-particles-js';
 import bannerImg from './assets/banner-bg3-fade.png';
 
 export default class Banner extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       modalOpen: false
@@ -25,7 +25,7 @@ export default class Banner extends React.Component {
     this.toggle = this.toggle.bind(this)
   }
 
-  toggle(){
+  toggle() {
     this.setState({
       modalOpen: !this.state.modalOpen
     })
@@ -34,127 +34,130 @@ export default class Banner extends React.Component {
   render() {
     return (
       <div>
-        <Particles
-          params={{
-            particles: {
-              number: {
-                value: 80,
-                density: {
-                  enable: true,
-                  value_area: 1600
-                }
-              },
-              color: {
-                value: '#ffffff'
-              },
-              shape: {
-                type: 'circle',
-                stroke: {
-                  width: 0,
-                  color: '#000000'
-                },
-                polygon: {
-                  nb_sides: 5
-                },
-                image: {
-                  src: 'img/github.svg',
-                  width: 100,
-                  height: 100
-                }
-              },
-              opacity: {
-                value: 0.5,
-                random: false,
-                anim: {
-                  enable: false,
-                  speed: 1,
-                  opacity_min: 0.1,
-                  sync: false
-                }
-              },
-              size: {
-                value: 10,
-                random: true,
-                anim: {
-                  enable: false,
-                  speed: 1,
-                  size_min: 0.1,
-                  sync: false
-                }
-              },
-              line_linked: {
-                enable: false,
-                distance: 150,
-                color: '#ffffff',
-                opacity: 0.4,
-                width: 1
-              },
-              move: {
-                enable: true,
-                speed: 2,
-                direction: 'none',
-                random: false,
-                straight: false,
-                out_mode: 'out',
-                bounce: false,
-                attract: {
-                  enable: false,
-                  rotateX: 600,
-                  rotateY: 1200
-                }
-              }
-            },
-            interactivity: {
-              detect_on: 'window',
-              events: {
-                onhover: {
-                  enable: true,
-                  mode: 'bubble'
-                },
-                onclick: {
-                  enable: false,
-                  mode: 'push'
-                },
-                resize: true
-              },
-              modes: {
-                grab: {
-                  distance: 400,
-                  line_linked: {
-                    opacity: 1
+        <div className='overlay'>
+          <Particles
+            params={{
+              particles: {
+                number: {
+                  value: 50,
+                  density: {
+                    enable: true,
+                    value_area: 1600
                   }
                 },
-                bubble: {
-                  distance: 400,
-                  size: 15,
-                  duration: 2,
-                  opacity: 0.7,
-                  speed: 3
+                color: {
+                  value: '#ffffff'
                 },
-                repulse: {
-                  distance: 200,
-                  duration: 0.4
+                shape: {
+                  type: 'circle',
+                  stroke: {
+                    width: 0,
+                    color: '#000000'
+                  },
+                  polygon: {
+                    nb_sides: 5
+                  },
+                  image: {
+                    src: 'img/github.svg',
+                    width: 100,
+                    height: 100
+                  }
                 },
-                push: {
-                  particles_nb: 4
+                opacity: {
+                  value: 0.5,
+                  random: false,
+                  anim: {
+                    enable: false,
+                    speed: 1,
+                    opacity_min: 0.1,
+                    sync: false
+                  }
                 },
-                remove: {
-                  particles_nb: 2
+                size: {
+                  value: 10,
+                  random: true,
+                  anim: {
+                    enable: false,
+                    speed: 1,
+                    size_min: 0.1,
+                    sync: false
+                  }
+                },
+                line_linked: {
+                  enable: false,
+                  distance: 150,
+                  color: '#ffffff',
+                  opacity: 0.4,
+                  width: 1
+                },
+                move: {
+                  enable: true,
+                  speed: 2,
+                  direction: 'top-right',
+                  random: false,
+                  straight: false,
+                  out_mode: 'out',
+                  bounce: false,
+                  attract: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 1200
+                  }
                 }
-              }
-            },
-            retina_detect: true
-          }} style={{
-            display: 'block',
-            position: 'relative',
-            zIndex: -1,
-            width: '100%',
-            height: '100%',
-            background: `url(${bannerImg}) no-repeat center center fixed`,
-            backgroundSize: 'cover'
-          }}
-          height={800}
-        />
+              },
+              interactivity: {
+                detect_on: 'window',
+                events: {
+                  onhover: {
+                    enable: true,
+                    mode: 'bubble'
+                  },
+                  onclick: {
+                    enable: false,
+                    mode: 'push'
+                  },
+                  resize: true
+                },
+                modes: {
+                  grab: {
+                    distance: 400,
+                    line_linked: {
+                      opacity: 1
+                    }
+                  },
+                  bubble: {
+                    distance: 400,
+                    size: 12,
+                    duration: 2,
+                    opacity: 0.7,
+                    speed: 3
+                  },
+                  repulse: {
+                    distance: 200,
+                    duration: 0.4
+                  },
+                  push: {
+                    particles_nb: 4
+                  },
+                  remove: {
+                    particles_nb: 2
+                  }
+                }
+              },
+              retina_detect: true
+            }} style={{
+              display: 'block',
+              position: 'relative',
+              zIndex: -1,
+              width: '100%',
+              height: '100%',
+              background: `url(${bannerImg}) no-repeat center center fixed`,
+              backgroundSize: 'cover'
+            }}
+            height={800}
+          />
+        </div>
+
         <Container className='banner-container'>
           <Row className='banner-row'>
             <Col md="6" className='align-self-center'>
