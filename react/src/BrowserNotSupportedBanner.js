02a306547e3fbfa63952cb65ec18fcf963c90232
Jimmy Messmer <james.messmer@vumc.org>
2018-11-07 13:56:55 -0600

    HANDELP-84

--- react/src/BrowserNotSupportedBanner.js
+++ react/src/BrowserNotSupportedBanner.js
@@ -8,7 +8,7 @@ export default class BrowserNotSupportedBanner extends React.Component {
   render() {
     return (
       <Container fluid className='browser-not-supported-banner'>
-        <h5>{this.props.browser.charAt(0).toUpperCase() + this.props.browser.slice(1)} is not supported. Please consider using the most recent versions of Mozilla Firefox or Google Chrome.</h5>
+        <h5>{this.props.browser.charAt(0).toUpperCase() + this.props.browser.slice(1)} is not supported. Please consider using the most recent versions of <a href='https://www.mozilla.org/en-US/firefox/new/'>Mozilla Firefox</a> or <a href="https://www.google.com/chrome/">Google Chrome</a>.</h5>
       </Container>
     )
   }
