19fd3bb9460f7cd81f2cfe632b5aaf1b88b91ce2
Jimmy Messmer <james.messmer@vumc.org>
2018-11-07 11:54:28 -0600

    HANDELP-84 Updated compatability list

--- react/src/App.js
+++ react/src/App.js
@@ -16,14 +16,64 @@ import BrowserNotSupportedBanner from './BrowserNotSupportedBanner'
 
 
 class App extends Component {
-  render() {
+
+  constructor(props){
+    super(props)
+    this.checkCompatability = this.checkCompatability.bind(this)
+  }
+
+  checkCompatability(){
     const { detect } = require('detect-browser')
     const browser = detect()
     var supported = true
-    console.log(`Browser: ${JSON.stringify(browser)}`)
-    if (browser.name.toLowerCase() !== 'firefox' && browser.name.toLowerCase() !== 'chrome'){
-      supported = false
+
+    switch(browser.name.toLowerCase()){
+      case 'firefox':
+        if (parseInt(browser.version.split('.')[0], 10) < 13){
+          supported = false;
+        }
+        break
+      case 'chrome':
+        if (parseInt(browser.version.split('.')[0], 10) < 38){
+          supported = false;
+        }
+        break
+      case 'ie':
+        if (parseInt(browser.version.split('.')[0], 10) < 11){
+          supported = false;
+        }
+        break
+      case 'safari':
+        if (parseInt(browser.version.split('.')[0], 10) < 8){
+          supported = false;
+        }
+        break
+      case 'opera':
+        if (parseInt(browser.version.split('.')[0], 10) < 25){
+          supported = false;
+        }
+        break
+      case 'edge':
+        if (parseInt(browser.version.split('.')[0], 10) < 12){
+          supported = false;
+        }
+        break
+      default:
+        supported = false;
     }
+
+    return {isSupported: supported, browserInfo: browser}
+
+  }
+
+  render() {
+    var supportInfo = this.checkCompatability();
+    var supported = supportInfo.isSupported
+    var browser = supportInfo.browserInfo
+    console.log(`Browser: ${JSON.stringify(supportInfo)}`)
+    // if (browser.name.toLowerCase() !== 'firefox' && browser.name.toLowerCase() !== 'chrome'){
+    //   supported = false
+    // }
     return (
       <div className='app'>
         {supported === false && <BrowserNotSupportedBanner browser={browser.name === 'ie' ? 'Internet Explorer' : browser.name} />}
