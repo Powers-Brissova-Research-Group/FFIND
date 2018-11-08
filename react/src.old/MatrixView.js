c189691645cb9166685b1dce5fc6f39f1a87d0b1
Jimmy Messmer <james.messmer@vumc.org>
2018-11-08 14:35:12 -0600

    HANDELP-85 Added container for progress bars without one

--- react/src.old/MatrixView.js
+++ react/src.old/MatrixView.js
@@ -130,10 +130,10 @@ export default class MatrixView extends React.Component {
       return <Error error_desc={this.state.error.message} />
     } else {
       return (
-        <div className="loading">
+        <Container className="loading">
           <strong>Loading {this.props.dataset_name}...</strong>
           <Progress animated color="success" value="100" />
-        </div>
+        </Container>
       )
     }
   }
