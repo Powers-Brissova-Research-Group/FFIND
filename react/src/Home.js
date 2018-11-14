f24dc989773af87c1731ada98643501cdd13ef22
JP Cartailler <c5creative@users.noreply.github.com>
2018-11-14 12:51:03 -0600

    quote consistency

--- react/src/Home.js
+++ react/src/Home.js
@@ -28,12 +28,12 @@ render() {
             <div className="head-description v-padded">
               <Row>
                 <Col md="12">
-                  <h2 className='section-heading'>Our Approach</h2>
+                  <h2 className="section-heading">Our Approach</h2>
                 </Col>
               </Row>
               <Row>
                 <Col md="12">
-                  <h5 className='section-desc'>Our investigators are studying how the human pancreas and islets change in the first decade of life. This website, an atlas of our results, seeks to make this new information available to scientists to accelerate research throughout the world with the goal of understanding of the events in the pancreas and islet that trigger type 1 diabetes.</h5>
+                  <h5 className="section-desc">Our investigators are studying how the human pancreas and islets change in the first decade of life. This website, an atlas of our results, seeks to make this new information available to scientists to accelerate research throughout the world with the goal of understanding of the events in the pancreas and islet that trigger type 1 diabetes.</h5>
                 </Col>
               </Row>
             </div>
@@ -50,9 +50,9 @@ render() {
             </Row>
           </Row>
         </Container>
-        <Container fluid className='shaded'>
+        <Container fluid className="shaded">
           <Container>
-            <Row className='v-padded'>
+            <Row className="v-padded">
               <Col md="6">
                 <h3>Creating new</h3>
                 <h1>Connections</h1>
@@ -61,47 +61,47 @@ render() {
                 <Button color="secondary">Learn about our collaborators and their work</Button>
               </Col>
               <Col md="6">
-                <img src={MapPicture} alt='Collaborators map' className='img-fill' />
+                <img src={MapPicture} alt="Collaborators map" className="img-fill" />
               </Col>
             </Row>
           </Container>
         </Container>
         <Container fluid>
-          <div className='support v-padded'>
-            <Row className='center-row'>
+          <div className="support v-padded">
+            <Row className="center-row">
               <Col sm="12">
                 <h2>Support</h2>
                 <p>This project is generously supported by:</p>
               </Col>
             </Row>
-            <Row className='center-row'>
+            <Row className="center-row">
               <Col sm="12">
-                <img src={Helmsley} alt='The Leona M. and Harry B. Helmsley Charitable Trust' />
+                <img src={Helmsley} alt="The Leona M. and Harry B. Helmsley Charitable Trust" />
               </Col>
             </Row>
-            <Row className='center-row'>
+            <Row className="center-row">
               <Col sm="12">
                 <p>We also thank the following</p>
               </Col>
             </Row>
             <Row>
               <Col sm="2">
-                <img src={VA} alt='VA' className='img-fill'  />
+                <img src={VA} alt="VA" className="img-fill" />
               </Col>
               <Col sm="2">
-                <img src={HIRN} alt='HIRN' className='img-fill'  />
+                <img src={HIRN} alt="HIRN" className="img-fill" />
               </Col>
               <Col sm="2">
-                <img src={IIAM} alt='IIAM' className='img-fill'  />
+                <img src={IIAM} alt="IIAM" className="img-fill" />
               </Col>
               <Col sm="2">
-                <img src={IIDP} alt='IIDP' className='img-fill'  />
+                <img src={IIDP} alt="IIDP" className="img-fill" />
               </Col>
               <Col sm="2">
-                <img src={VUMC} alt='VUMC' className='img-fill'  />
+                <img src={VUMC} alt="VUMC" className="img-fill" />
               </Col>
               <Col sm="2">
-                <img src={NIDDK} alt='NIDDK' className='img-fill'  />
+                <img src={NIDDK} alt="NIDDK" className="img-fill" />
               </Col>
             </Row>
           </div>
