dc26b1f014e95bd375a36f4c8297fb78d54b093f
Jimmy Messmer <jmessmer@live.unc.edu>
2019-04-03 16:28:17 -0500

    Wired up dataset overview page to rest of application

--- react/src/pancreatlas/DatasetOverview.js
+++ react/src/pancreatlas/DatasetOverview.js
@@ -8,16 +8,23 @@ import {
   Button
 } from 'reactstrap'
 
+import {
+  Link
+} from 'react-router-dom'
+
 import { Parallax } from 'react-parallax'
 
 export default class DatasetOverview extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
+    let re = /(\/\w+\/?)+([0-9]+)(\/\w+\/?)+/
+
     this.state = {
       title: 'Default Title',
-      desc: 'Default desc'
+      desc: 'Default desc',
+      did: (re.exec(window.location.pathname) !== null) ? re.exec(window.location.pathname)[2] : 0,
+      funders: []
     }
-    let re = /(\/\w+\/?)+([0-9]+)(\/\w+\/?)+/
     let dsid = (re.exec(window.location.pathname) !== null) ? re.exec(window.location.pathname)[2] : 0
     console.log(dsid)
     window.fetch(`${process.env.REACT_APP_API_URL}/datasets/${dsid}`, {
@@ -30,15 +37,33 @@ export default class DatasetOverview extends React.Component {
     })
       .then(res => res.json())
       .then((result) => {
-        console.log(result)
+        const images = require.context('../assets/', true)
+        // this.props.funding !== undefined ? this.props.funding.split(',').map(source => images(`./${source}.jpg`)) : []
+        let sponsors = result.kvals.funding !== undefined ? result.kvals.funding.split(',').map(source => images(`./${source}.jpg`)) : []
+        console.log(sponsors)
         this.setState({
           title: result.dsname,
           desc: result.kvals.description_long,
-          funders: result.kvals.funding.split(',')
+          funders: sponsors
         })
       })
   }
-  render () {
+  render() {
+    /*
+                                    <Link to={{ pathname: `/pancreatlas/dataset/${item.did}`, search: '?browse=false' }}>
+                                      <Button className='ds-list-left-button' >Browse All Images</Button>
+                                    </Link>
+                                    <Link to={{ pathname: `/pancreatlas/dataset/${item.did}`, search: '?browse=true' }}>
+                                      <Button color='primary'>Browse by Age</Button>
+                                    </Link>
+                                    <Link to={'/pancreatlas/matrixview/' + item.did}>
+                                      <Button className='ds-list-right-button' outline color='success'>Compare Attributes</Button>
+                  import {
+  Link
+} from 'react-router-dom'
+                  </Link>
+
+    */
     return (
       <div className='datasetOverviewWrapper'>
         <Parallax
@@ -66,11 +91,11 @@ export default class DatasetOverview extends React.Component {
           </div>
         </Parallax>
         <Container>
-          <Row style={{ height: '50vh' }}>
+          <Row>
             <Col md='12'>
               <Row>
                 <Col md='12'>
-                  <h1>Explore the Data</h1>
+                  <h1><strong>Explore the Data</strong></h1>
                   <h3>Here are some suggested projections of the data within this set</h3>
                 </Col>
               </Row>
@@ -80,7 +105,9 @@ export default class DatasetOverview extends React.Component {
                     <CardBody>
                       <h3>Browse by Age</h3>
                       <p>Choose a specific age range of donors within which to view samples</p>
-                      <Button>Browse</Button>
+                      <Link to={{ pathname: `/pancreatlas/dataset/${this.state.did}`, search: '?browse=true' }}>
+                        <Button>Browse</Button>
+                      </Link>
                     </CardBody>
                   </Card>
                 </Col>
@@ -89,7 +116,9 @@ export default class DatasetOverview extends React.Component {
                     <CardBody>
                       <h3>Browse by Matrix</h3>
                       <p>Create a two-dimensional matrix comparing specified attribute sets to find data matching specific criteria</p>
-                      <Button>Browse</Button>
+                      <Link to={`/pancreatlas/matrixview/${this.state.did}`}>
+                        <Button>Browse</Button>
+                      </Link>
                     </CardBody>
                   </Card>
                 </Col>
@@ -98,17 +127,26 @@ export default class DatasetOverview extends React.Component {
                     <CardBody>
                       <h3>View All Images</h3>
                       <p>Don't restrict the data by any filters and view the entire collection</p>
-                      <Button>Browse</Button>
+                      <Link to={{ pathname: `/pancreatlas/dataset/${this.state.did}`, search: '?browse=false' }}>
+                        <Button>Browse</Button>
+                      </Link>
                     </CardBody>
                   </Card>
                 </Col>
               </Row>
             </Col>
           </Row>
-          <Row style={{ height: '50vh' }}>
+          <Row>
             <Col md='12'>
               <h1>We thank the following sponsors who made gathering these data possible:</h1>
             </Col>
+          </Row>
+          <Row>
+            {this.state.funders.map(funder => (
+              <Col md='4'>
+                <img src={funder} />
+              </Col>
+            ))}
           </Row>
         </Container>
       </div>
