97aed7e5aa0f63bbe628959c066f99426a4a6d65
Jimmy Messmer <jmessmer@live.unc.edu>
2019-04-04 09:41:22 -0500

    Fixed test for DatasetOverview

--- react/src/tests/DatasetOverview.test.js
+++ react/src/tests/DatasetOverview.test.js
@@ -2,8 +2,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import DatasetOverview from '../pancreatlas/DatasetOverview'
+import { MemoryRouter } from 'react-router-dom'
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<DatasetOverview />, div)
+  ReactDOM.render(<MemoryRouter><DatasetOverview /></MemoryRouter>, div)
 })
