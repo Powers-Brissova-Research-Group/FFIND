37cd9070c4bd51926dbf7e958aa969c5b405a867
Jimmy Messmer <jmessmer@live.unc.edu>
2019-04-17 15:22:41 -0500

    Simplified DatasetCard

--- react/src/pancreatlas/DatasetCard.js
+++ react/src/pancreatlas/DatasetCard.js
@@ -14,6 +14,8 @@ import {
   DropdownItem
 } from 'reactstrap'
 
+import { Collapse } from 'react-collapse'
+
 import {
   Link
 } from 'react-router-dom'
@@ -23,9 +25,11 @@ export default class DatasetCard extends React.Component {
     super(props)
 
     this.toggleDropdown = this.toggleDropdown.bind(this)
+    this.revealActions = this.revealActions.bind(this)
 
     this.state = {
-      dropdownOpen: false
+      dropdownOpen: false,
+      showActions: false
     }
   }
 
@@ -35,8 +39,14 @@ export default class DatasetCard extends React.Component {
     })
   }
 
+  revealActions () {
+    this.setState({
+      showActions: !this.state.showActions
+    })
+  }
+
   render () {
-    let sponsors = this.props.funding !== undefined ? this.props.funding.split(',').map(source => require(`../assets/${source}.jpg`)) : []
+    // let sponsors = this.props.funding !== undefined ? this.props.funding.split(',').map(source => require(`../assets/${source}.jpg`)) : []
     let logo = 'http://www.placehold.it/326x116'
     try {
       logo = require(`../assets/pancreatlas/logos/${this.props.title.toLowerCase().replace(/ /g, '-')}.png`)
@@ -44,48 +54,46 @@ export default class DatasetCard extends React.Component {
       console.log('Cannot find logo')
     }
     return (
-      <Card className='h-100'>
-        <CardImg className='ds-logo' src={logo} alt='placeholder' />
-        <CardBody className='text-left'>
-          <div className='ds-alt'><CardText>{this.props.description}</CardText></div>
-          <div className='dataset-card-sponsors'>
-            {sponsors.map(item =>
-              <Col key={`${item}-col`} md='6'>
-                <img className='dataset-funder' src={item} alt={item} />
-              </Col>
-            )}
-          </div>
-        </CardBody>
-        <CardFooter>
-          <Row className='mb-2'>
-            <Col md='12'>
-              <div className='dataset-card-buttons'>
-                <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropdown}>
-                  <DropdownToggle color='info' caret>
+      <div className='dataset-card' onMouseEnter={this.revealActions} onMouseLeave={this.revealActions}>
+        <Card className='h-100'>
+          <CardImg className='ds-logo' src={logo} alt='placeholder' />
+          <CardBody className='text-left'>
+            <div className='ds-alt'><CardText>{this.props.description}</CardText></div>
+            <Link to={{ pathname: `/pancreatlas/dataset/${this.props.did}`, search: '?browse=false' }}><Button className='w-100' color='info'>Browse All Images</Button></Link>
+          </CardBody>
+          <Collapse isOpened={this.state.showActions}>
+            <CardFooter>
+              <Row className='mb-2'>
+                <Col md='12'>
+                  <div className='dataset-card-buttons'>
+                    <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropdown}>
+                      <DropdownToggle outline color='secondary' caret>
                     Viewing Options
-                  </DropdownToggle>
-                  <DropdownMenu>
-                    <DropdownItem>
-                      <Link to={{ pathname: `/pancreatlas/dataset/${this.props.did}`, search: '?browse=false' }}>Browse All Images</Link>
-                    </DropdownItem>
-                    <DropdownItem>
-                      <Link to={{ pathname: `/pancreatlas/dataset/${this.props.did}`, search: '?browse=true' }}>Browse Images by Age</Link>
-                    </DropdownItem>
-                    <DropdownItem>
-                      <Link to={'/pancreatlas/matrixview/' + this.props.did}>
+                      </DropdownToggle>
+                      <DropdownMenu>
+                        <DropdownItem>
+                          <Link to={{ pathname: `/pancreatlas/dataset/${this.props.did}`, search: '?browse=false' }}>Browse All Images</Link>
+                        </DropdownItem>
+                        <DropdownItem>
+                          <Link to={{ pathname: `/pancreatlas/dataset/${this.props.did}`, search: '?browse=true' }}>Browse Images by Age</Link>
+                        </DropdownItem>
+                        <DropdownItem>
+                          <Link to={'/pancreatlas/matrixview/' + this.props.did}>
                     Browse Images via Matrix
-                      </Link>
-                    </DropdownItem>
-                  </DropdownMenu>
-                </ButtonDropdown>
-                <Link to={`/pancreatlas/dataset/${this.props.did}/overview`}>
-                  <Button>Collection  Details</Button>
-                </Link>
-              </div>
-            </Col>
-          </Row>
-        </CardFooter>
-      </Card>
+                          </Link>
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </ButtonDropdown>
+                    <Link to={`/pancreatlas/dataset/${this.props.did}/overview`}>
+                      <Button outline>Collection  Details</Button>
+                    </Link>
+                  </div>
+                </Col>
+              </Row>
+            </CardFooter>
+          </Collapse>
+        </Card>
+      </div>
     )
   }
 }
