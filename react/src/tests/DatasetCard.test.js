96f2c40f22f4a1f05285aa99cf4bdfb596660311
Jimmy Messmer <jmessmer@live.unc.edu>
2019-05-02 15:04:36 -0500

    Added more unit tests

--- react/src/tests/DatasetCard.test.js
+++ react/src/tests/DatasetCard.test.js
@@ -1,9 +1,32 @@
+/* eslint-env jest */
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import { MemoryRouter } from 'react-router-dom'
+import { DropdownToggle, Button } from 'reactstrap'
+
+import { mount } from 'enzyme'
+
 import DatasetCard from '../pancreatlas/DatasetCard'
 
-xit('renders without crashing', () => {
+it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<DatasetCard />, div)
+  ReactDOM.render(<MemoryRouter><DatasetCard /></MemoryRouter>, div)
+})
+
+describe('buttons', () => {
+  let wrapper = null
+
+  beforeEach(() => {
+    wrapper = mount(<MemoryRouter><DatasetCard /></MemoryRouter>)
+  })
+
+  it('Dropdown works', () => {
+    let button = wrapper.find(DropdownToggle).find(Button)
+    button.simulate('click')
+    wrapper.update()
+    let dscard = wrapper.find(DatasetCard)
+    expect(dscard.state().dropdownOpen).toBe(true)
+  })
 })
--- react/src/tests/DetailRow.test.js
+++ react/src/tests/DetailRow.test.js
@@ -1,9 +1,36 @@
+/* eslint-env jest */
 
 import React from 'react'
 import ReactDOM from 'react-dom'
 import DetailRow from '../pancreatlas/DetailRow'
 
+import { Table } from 'reactstrap'
+
+import { mount } from 'enzyme'
+
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<DetailRow />, div)
+})
+
+describe('Loads descriptions properly', () => {
+  let wrapper = null
+  let defs = require('../assets/pancreatlas/definitions.json')
+
+  beforeEach(() => {
+    const div = document.createElement('div')
+    div.setAttribute('id', 'TooltipDiv')
+    document.body.appendChild(div)
+    wrapper = mount(<Table><tbody><DetailRow data='Sagittal' desc={defs['Image Tags']['Image info - Section Plane'].short_desc} heading='Section Plane' /></tbody></Table>, { attachTo: div })
+  })
+
+  it('Contains tags', () => {
+    expect(defs['Image Tags']['Image info - Section Plane']).not.toBe(undefined)
+  })
+
+  it('Mounts properly', () => {
+    let detailRow = wrapper.find(DetailRow).instance()
+    detailRow.setState({ ttOpen: true })
+    expect(detailRow.state.ttOpen).toBe(true)
+  })
 })
