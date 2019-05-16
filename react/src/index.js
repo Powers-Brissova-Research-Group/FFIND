2f8fa4764a5ddcf67f164011aa25ec184d2c226b
Jimmy Messmer <jmessmer@live.unc.edu>
2019-05-16 16:16:55 -0500

    Hopefully fixed caching issue
    Found out that the service worker in React was causing the problem with caches staying way too long..removed that to hopefully fix the issue.

--- react/src/index.js
+++ react/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+import unregister from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+unregister()
