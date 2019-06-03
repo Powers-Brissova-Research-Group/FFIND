8b2e5a3ad5ba1b2e7914690cbd533ceb3c41f790
Jimmy <james.messmer@vumc.org>
2019-06-03 16:40:43 -0500

    [#4] Re-enabling serviceWorker

--- react/src/index.js
+++ react/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
-// import unregister from './registerServiceWorker'
+import register from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-// unregister()
+register()
