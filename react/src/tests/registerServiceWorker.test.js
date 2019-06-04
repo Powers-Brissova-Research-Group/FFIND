6ee830a6088e9268ed485c6c6ffdffa16a97bce3
Jimmy <james.messmer@vumc.org>
2019-06-04 15:05:18 -0500

    [#4] Fixed failing test from previous commit

--- react/src/tests/registerServiceWorker.test.js
+++
@@ -1,9 +0,0 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-import registerServiceWorker from '../pancreatlas/registerServiceWorker'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<registerServiceWorker />, div)
-})
