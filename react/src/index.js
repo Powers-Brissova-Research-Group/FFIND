ef49db0b0f42a0b28a7014f065b435d8648307a8
Jimmy <james.messmer@vumc.org>
2019-06-04 14:56:51 -0500

    [#4] Disabling serviceWorker again

--- react/src/index.js
+++ react/src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
-import register from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-register()
