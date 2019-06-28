c0133f1d92d9349f203f16c7100f89470c90a4fa
Jimmy <james.messmer@vumc.org>
2019-06-28 10:59:53 -0500

    [#6] Fixed failing test

--- react/src/tests/About.test.js
+++ react/src/tests/About.test.js
@@ -2,8 +2,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import About from '../About'
+import { MemoryRouter } from 'react-router-dom'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<About />, div)
+  ReactDOM.render(<MemoryRouter><About /></MemoryRouter>, div)
 })
