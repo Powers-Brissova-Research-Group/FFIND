c4660d9c885c853373896d5e697c1fa0b17e38e7
Jimmy <james.messmer@vumc.org>
2019-07-01 14:05:21 -0500

    [#19] Updated collaborators
    Updated collaborators page with new text and look. Also removed Chunhua

--- react/src/Collaborators.js
+++ react/src/Collaborators.js
@@ -37,32 +37,75 @@ export default class Collaborators extends React.Component {
         <Container fluid className='shaded'>
 
           <Container className='v-padded'>
-            <h2><span>Meet the investigators</span></h2>
+            <h2 className='mb-3'><span>Meet the investigators</span></h2>
             <Row className='mt-2'>
               <Col md='4'>
-                <TeamMember name='Mark Atkinson' institution='University of Florida' desc='Our laboratory seeks to directly define methods for disease prevention in non-diabetic subjects identified to be at increased risk for the disease or diabetic subjects through pancreatic transplantation in association with novel forms of immunotherapy.' imgSrc='https://dev8-labnodes.app.vanderbilt.edu/assets/handelp/media/mark-atkinson.jpg' site='https://diabetes.ufl.edu/atkinson-lab/' email='mailto:atkinson@pathology.ufl.edu' phone='tel:352-273-8277' />
+                <TeamMember
+                  name='Mark Atkinson'
+                  institution='University of Florida'
+                  imgSrc='https://dev8-labnodes.app.vanderbilt.edu/assets/handelp/media/mark-atkinson.jpg'
+                  site='https://diabetes.ufl.edu/atkinson-lab/'
+                  email='mailto:atkinson@pathology.ufl.edu'
+                  phone='tel:352-273-8277'>
+                    Dr. Atkinson is a longtime champion of type 1 diabetes research, serving the community in numerous leadership roles throughout his career. His lab seeksto directly define methods for disease prevention in non-diabetic subjects identified to be at increased risk for the disease, in association with novel forms of immunotherapy.
+                </TeamMember>
               </Col>
               <Col md='4'>
-                <TeamMember name='Rita Bottino' institution='Allegheny General Hospital, Pittsburgh' imgSrc='https://dev8-labnodes.app.vanderbilt.edu/assets/handelp/media/rita-bottino.jpg' site='https://www.ahn.org/research/our-research-institutes/cellular-therapeutics/our-team' email='mailto:rita.bottino@ahn.org' phone='tel:412-512-6496' desc='Our team is a reference laboratory for the isolation and distribution is islets of Langerhans obtained from the pancreas of deceased organ donors for research purposes' />
+                <TeamMember
+                  name='Rita Bottino'
+                  institution='Allegheny Health Network'
+                  imgSrc='https://dev8-labnodes.app.vanderbilt.edu/assets/handelp/media/rita-bottino.jpg'
+                  site='https://www.ahn.org/research/our-research-institutes/cellular-therapeutics/our-team'
+                  email='mailto:rita.bottino@ahn.org'
+                  phone='tel:412-512-6496'>
+                    Dr. Bottino leads a world-class islet isolation center that provides human islets for both clinical transplants and research. Her highly experienced team has established reference protocols and best practices for human islet isolation, processing thousands of organs with an array of disease phenotypes.
+                </TeamMember>
               </Col>
               <Col md='4'>
-                <TeamMember name='Marcela Brissova' institution='Vanderbilt University Medical Center' imgSrc='https://dev8-labnodes.app.vanderbilt.edu/assets/handelp/media/marcela-brissova.jpg' site='https://medicine.mc.vanderbilt.edu/person/marcela-brissova-phd' email='mailto:marcela.brissova@vanderbilt.edu' phone='tel:615-936-1729' desc='Our research interests include the biology of pancreatic islets with a focus on the regulation of beta cell gene expression to provide insight into processes controlling normal pancreatic beta cell function in rodent models and translating this research to human islet biology.' />
+                <TeamMember
+                  name='Marcela Brissova'
+                  institution='Vanderbilt University'
+                  imgSrc='https://dev8-labnodes.app.vanderbilt.edu/assets/handelp/media/marcela-brissova.jpg'
+                  site='https://medicine.mc.vanderbilt.edu/person/marcela-brissova-phd'
+                  email='mailto:marcela.brissova@vanderbilt.edu'
+                  phone='tel:615-936-1729'>
+                    Dr. Brissova directs the Human Islet Phenotyping Program (HIPP) of the <a href='https://iidp.coh.org/'>Integrated Islet Distribution Program (IIDP)</a>, with particular expertise in functional islet phenotyping and immunohistochemistry. Her research interests include human pancreatic development, islet vascularization, and translation of transgenic mouse models to human islet biology.
+                </TeamMember>
               </Col>
             </Row>
             <Row className='mt-2'>
               <Col md='4'>
-                <TeamMember name='Chunhua Dai' institution='Vanderbilt University Medical Center' imgSrc='https://dev8-labnodes.app.vanderbilt.edu/assets/handelp/media/chunhua-dai.jpg' site='https://www.powersresearch.org/people' email='mailto:chunhua.dai@vanderbilt.edu' phone='tel:615-936-7678' desc='Our research is focused on the molecular, cellular, and vascular changes in human islets when they are challenged with hyperglycemia and/or insulin resistance in vivo.' />
+                <TeamMember
+                  name='Seung Kim'
+                  institution='Stanford University'
+                  imgSrc='https://dev8-labnodes.app.vanderbilt.edu/assets/handelp/media/seung-kim.jpg'
+                  site='https://seungkimlab.stanford.edu/'
+                  email='mailto:seungkim@stanford.edu'
+                  phone='tel:650-723-6230'>
+                    Dr. Kim leads diabetes research efforts at Stanford, having pioneered numerous approaches to create, expand, and regenerate islets. Dr. Kim’s group performs sophisticated molecular and cellular analyses to elucidate mechanisms that underlie pancreatic development and growth, with the aim of developing diagnostic and therapeutic paradigms for human disease.
+                </TeamMember>
               </Col>
               <Col md='4'>
-                <TeamMember name='Seung Kim' institution='Stanford University' imgSrc='https://dev8-labnodes.app.vanderbilt.edu/assets/handelp/media/seung-kim.jpg' site='https://seungkimlab.stanford.edu/' email='mailto:seungkim@stanford.edu' phone='tel:650-723-6230' desc='The Kim lab studies islet and pancreas biology with the aim of developing diagnostic and therapeutic paradigms for human diseases.' />
+                <TeamMember
+                  name='Al Powers'
+                  institution='Vanderbilt University'
+                  imgSrc='https://dev8-labnodes.app.vanderbilt.edu/assets/handelp/media/al-powers.jpg'
+                  site='https://www.powersresearch.org/'
+                  email='mailto:al.powers@vanderbilt.edu'
+                  phone='tel:615-936-7678'>
+                    Dr. Powers, a president emeritus of the American Diabetes Association (ADA), is committed to cross-institutional efforts for integrated, multi-modal analyses of donor pancreata. His group seeks to understand and reverse &beta; cell and islet dysfunction in type 1 and type 2 diabetes, studying human islets in both <em>in vitro</em> and <em>in vivo</em> settings.
+                </TeamMember>
               </Col>
               <Col md='4'>
-                <TeamMember name='Al Powers' institution='Vanderbilt University Medical Center' imgSrc='https://dev8-labnodes.app.vanderbilt.edu/assets/handelp/media/al-powers.jpg' site='https://www.powersresearch.org/' email='mailto:al.powers@vanderbilt.edu' phone='tel:615-936-7678' desc='The Powers lab seeks to understand and reverse β-cell and islet abnormalities and dysfunction in type 1 and type 2 diabetes.' />
-              </Col>
-            </Row>
-            <Row className='mt-2'>
-              <Col md='4'>
-                <TeamMember name='Chris Wright' institution='Vanderbilt University Medical Center' imgSrc='https://dev8-labnodes.app.vanderbilt.edu/assets/handelp/media/chris-wright.jpg' site='https://lab.vanderbilt.edu/wright-lab/' email='mailto:chris.wright@vanderbilt.Edu' phone='tel:615-343-8256' desc='The Wright lab focuses on early embryonic development and organogenesis.' />
+                <TeamMember
+                  name='Chris Wright'
+                  institution='Vanderbilt University'
+                  imgSrc='https://dev8-labnodes.app.vanderbilt.edu/assets/handelp/media/chris-wright.jpg'
+                  site='https://lab.vanderbilt.edu/wright-lab/'
+                  email='mailto:chris.wright@vanderbilt.Edu'
+                  phone='tel:615-343-8256'>
+                    Dr. Wright leads a research group focused on early embryonic development and organogenesis, with particular interests in cell ontogeny and plasticity. Employing a combination of molecular and genetic techniques, his team works to detail intracellular signaling pathways critical to pancreas formation and maintenance.
+                </TeamMember>
               </Col>
             </Row>
           </Container>
--- react/src/TeamMember.js
+++ react/src/TeamMember.js
@@ -17,14 +17,14 @@ export default class TeamMember extends React.Component {
       <Card className='h-100'>
         <a href={this.props.site}><CardImg top width='100%' src={this.props.imgSrc} /></a>
         <CardBody className='d-flex flex-column'>
-          <CardTitle className='center-row'>{this.props.name}</CardTitle>
+          <CardTitle className='center-row collaborator-title'>{this.props.name}</CardTitle>
           <CardSubtitle className='center-row'>{this.props.institution}</CardSubtitle>
-          <CardText className='mt-2'>{this.props.desc}</CardText>
+          <CardText className='mt-2'>{this.props.children}</CardText>
         </CardBody>
-        <CardFooter className='d-flex justify-content-around'>
-          <a href={this.props.site}><FontAwesomeIcon size='1x' icon='link' /></a>
-          <a href={this.props.email}><FontAwesomeIcon size='1x' icon='envelope' /></a>
-          <a href={this.props.phone}><FontAwesomeIcon size='1x' icon='phone' /></a>
+        <CardFooter className='d-flex justify-content-end'>
+          <a className='ml-4' href={this.props.site}><FontAwesomeIcon size='1x' icon='link' /></a>
+          <a className='ml-4' href={this.props.email}><FontAwesomeIcon size='1x' icon='envelope' /></a>
+          <a className='ml-4' href={this.props.phone}><FontAwesomeIcon size='1x' icon='phone' /></a>
         </CardFooter>
       </Card>
     )
