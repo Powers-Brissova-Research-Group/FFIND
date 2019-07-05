52f1490b572f5b27239e7593c98a4c7666573161
Jimmy <james.messmer@vumc.org>
2019-07-05 16:28:38 -0500

    Fixed failing test

--- react/src/About.js
+++ react/src/About.js
@@ -22,16 +22,17 @@ export default class About extends React.Component {
         {/* <Header heading="About" /> */}
         <PageBanner image bgImg={require('./assets/about-header.jpg')}>
           <h1>About</h1>
+          <p className='text-larger'>Our goal is to organize and make accessible a subset of the rich, full-resolution images of human pancreas samples. By connecting images from several independent programs and efforts, we have created a practical resource to elevate and accelerate scientific study of the human pancreas.</p>
         </PageBanner>
         <Container className='v-padded'>
-          <Row>
+          <Row className='mb-4'>
             <Col md='12'>
               <h1>About Pancreatlas</h1>
               <p> Immunohistochemical analyses have been performed on the human pancreas for over a decade, but until now the field has lacked a central image repository. Pancreatlas was born out of the desire to share full-resolution, multi-channel imaging data – and our hope is that it becomes a practical and useful resource for researchers around the world.</p>
               <p>Images are housed within Collections that are organized by specific disease phenotype, biological event, or scientific program. Where possible, we have provided the appropriate citations, tissue processing details, and authorship. All images include metadata in standardized nomenclature that you can read more about <Link to='/nomenclature'>here</Link>.</p>
             </Col>
           </Row>
-          <Row>
+          <Row className='mb-4'>
             <Col md='12'>
               <h1 className='mt-4'>The Pancreatlas Team</h1>
               <Row className='mb-4'>
@@ -66,17 +67,28 @@ export default class About extends React.Component {
                   <img className='img-fluid rounded-circle team-member-img' src={require('./assets/pancreatlas/credits/marcela.png')} alt='Marcela Brissova' />
                 </Col>
                 <Col md='8' className='mb-auto mt-auto'>
-                  <h4>Marcela Brissova, Ph.D. &bull; <em>Director</em></h4>
+                  <h4>Marcela Brissova, Ph.D. &bull; <em>Scientific Director</em></h4>
                   <p>Marcela’s expertise and leadership in the field of human pancreas and islet biology has been essential to the creation of Pancreatlas. She is the driver of important organizational decisions and overall direction, and she serves as the interface between a number of collaborative efforts to share scientific data and knowledge about the human pancreas.</p>
                 </Col>
               </Row>
             </Col>
           </Row>
-          <Row>
+          <Row className='mb-4'>
             <Col md='12'>
               <h1>Credits</h1>
               <p>Pancreas and islet schematics on the homepage are courtesy of <strong>Rachel Chandler</strong>, a talented biomedical illustrator who worked with our team at Vanderbilt. <strong>Diane Saunders</strong> created the Pancreatlas logo.</p>
               <p>Pancreatlas depends on the OMERO-Plus software licensed from <a href='https://wwww.glencoesoftware.com'>Glencoe Software</a>. We are especially grateful to <strong>Dr. Jason Swedlow</strong> and <strong>Emil Rozbicki</strong> of Glencoe for their technical expertise and collaboration.</p>
+            </Col>
+          </Row>
+          <Row className='mb-4'>
+            <Col md='12'>
+              <h1>FAQ</h1>
+              <p><strong>Do I need an account to view/access images?</strong></p>
+              <p className='mb-4'>No registration is required to access images. We are committed to making data publicly available.</p>
+              <p><strong>Can I download images for analysis?</strong></p>
+              <p className='mb-4'>At this time, we do not have the capability to offer downloads. If you are interested in seeing this feature added in a future version of Pancreatlas, please send your feedback <a href='mailto:diane.saunders@vumc.org'>here</a>.</p>
+              <p><strong>Can I upload my own images to Pancreatlas?</strong></p>
+              <p>Pancreatlas is a curated resource, and since our goal is to provide a trustworthy reference for human pancreatic architecture, we have chosen to limit content accordingly. If you have images that you feel would be appropriate, we would love to discuss the possibility of including them. Please <a href='mailto:diane.saunders@vumc.org'>get in touch</a> for more information.</p>
             </Col>
           </Row>
         </Container>
--- react/src/tests/Collaborators.test.js
+++ react/src/tests/Collaborators.test.js
@@ -2,8 +2,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Collaborators from '../Collaborators'
+import { MemoryRouter } from 'react-router-dom'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<Collaborators />, div)
+  ReactDOM.render(<MemoryRouter><Collaborators /></MemoryRouter>, div)
 })
