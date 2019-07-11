c8c2185f30fcf6b3617878fff08deb98b7449f3a
Jimmy <james.messmer@vumc.org>
2019-07-11 15:23:39 -0500

    Fixed failing AgeBrowser test

--- react/src/tests/AgeBrowser.test.js
+++ react/src/tests/AgeBrowser.test.js
@@ -1,9 +1,28 @@
-
+/* eslint-env jest */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import AgeBrowser from '../pancreatlas/AgeBrowser'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<AgeBrowser />, div)
+import axios from 'axios'
+
+jest.mock('axios')
+
+describe('loads data properyly', () => {
+  let dataset =
+    {
+      'did': 410,
+      'dsname': 'Cystic Fibrosis-Related Diabetes (CFRD)',
+      'desc': 'Altered pancreatic architecture in the context of cystic fibrosis',
+      'kvals': {
+        'active': 'true'
+      }
+    }
+
+  const mockResp = { data: dataset }
+  axios.get.mockImplementation(() => Promise.resolve(mockResp))
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<AgeBrowser match={{ params: { dsid: 410 } }} />, div)
+  })
 })
