cddfd5e7f6fc4b331e76ee7ae72e12ba72274c3a
Jimmy <james.messmer@vumc.org>
2019-07-11 15:10:03 -0500

    Fixed failing matrixview test

--- react/src/tests/MatrixView.test.js
+++ react/src/tests/MatrixView.test.js
@@ -1,9 +1,30 @@
 
+/* eslint-env jest */
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import MatrixView from '../pancreatlas/MatrixView'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<MatrixView match={{ params: { dsid: 410 } }} />, div)
+import axios from 'axios'
+
+jest.mock('axios')
+
+describe('loads data properyly', () => {
+  let dataset =
+    {
+      'did': 410,
+      'dsname': 'Cystic Fibrosis-Related Diabetes (CFRD)',
+      'desc': 'Altered pancreatic architecture in the context of cystic fibrosis',
+      'kvals': {
+        'active': 'true'
+      }
+    }
+
+  const mockResp = { data: dataset }
+  axios.get.mockImplementation(() => Promise.resolve(mockResp))
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<MatrixView match={{ params: { dsid: 410 } }} />, div)
+  })
 })
