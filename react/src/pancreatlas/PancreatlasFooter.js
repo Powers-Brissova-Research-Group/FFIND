e7730e490c228c786c6c31b7fc64c5f035cacbbc
Jimmy <james.messmer@vumc.org>
2019-07-26 13:30:12 -0500

    Diane Edits 1

--- react/src/pancreatlas/PancreatlasFooter.js
+++ react/src/pancreatlas/PancreatlasFooter.js
@@ -28,8 +28,8 @@ export default class PancreatlasFooter extends React.Component {
           <Container>
             <Row className='v-padded'>
               <Col md='6'>
-                <p>Pancreatlas&trade; is an online resource that houses reference datasets from human pancreas samples, achieved through the generosity of organ donors and their families.</p>
-                <p>This website is supported by <strong><a href='http://www.helmsleytrust.org'>The Leona M. and Harry B. Helmsley Charitable Trust</a></strong>, <a href='https://www.jdrfnpod.org'>nPOD</a>, <a href='http://www.hirnetwork.org'>HIRN</a>, <a href='http://www.jdrf.org'>JDRF</a>, <a href='http://www.iiam.org'>IIAM</a>, <a href='http://www.ndri.org'>NDRI</a>, <a href='https://iidp.coh.org'>IIDP</a>, <a href='https://www.nih.gov'>NIH</a>, <a href='http://www.research.va.gov'>VA</a>, and <a href='https://www.mc.vanderbilt.edu'>VUMC</a>.</p>
+                <p className='pt-4'>Pancreatlas&trade; is an online resource that houses reference datasets from human pancreas samples, achieved through the generosity of organ donors and their families.</p>
+                <p>Featured research projects are supported by <strong><a href='http://www.helmsleytrust.org'>The Leona M. and Harry B. Helmsley Charitable Trust</a></strong>, <a href='https://www.jdrfnpod.org'>nPOD</a>, <a href='http://www.hirnetwork.org'>HIRN</a>, <a href='http://www.jdrf.org'>JDRF</a>, <a href='http://www.iiam.org'>IIAM</a>, <a href='http://www.ndri.org'>NDRI</a>, <a href='https://iidp.coh.org'>IIDP</a>, <a href='https://www.nih.gov'>NIH</a>, <a href='http://www.research.va.gov'>VA</a>, and <a href='https://www.mc.vanderbilt.edu'>VUMC</a>.</p>
               </Col>
 
               <Col className='float-right' md='6'>
@@ -37,9 +37,6 @@ export default class PancreatlasFooter extends React.Component {
                   <h2 className='get-in-touch'>Contact Us</h2>
                   <p>
                   Vanderbilt University Medical Center<br />
-                  8435F Medical Research Bldg IV<br />
-                  2215 Garland Avenue<br />
-                  Nashville, TN 37232-0475<br />
                   Phone: (615) 936-7678<br />
                   E-mail: <a href='mailto:powers.research@vanderbilt.edu'>powers-research@vanderbilt.edu</a></p>
                 </div>
