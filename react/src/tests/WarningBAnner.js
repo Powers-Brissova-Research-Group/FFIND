9f11bd6347a957f880fda421203897ff7d006b13
Jimmy <james.messmer@vumc.org>
2019-09-04 15:35:13 -0500

    [#34] Fixed failing test

--- react/src/tests/WarningBAnner.js
+++ react/src/tests/WarningBAnner.js
@@ -1,9 +1,9 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import BrowserNotSupportedBanner from '../BrowserNotSupportedBanner'
+import WarningBanner from '../WarningBanner'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<BrowserNotSupportedBanner />, div)
+  ReactDOM.render(<WarningBanner />, div)
 })
