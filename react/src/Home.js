b1f180a6c1e8cf1e4f36ed269334bbb695b0af67
Jimmy Messmer <james.messmer@vumc.org>
2020-03-17 10:31:51 -0500

    [#81] Fixed image resizing issue

--- react/src/Home.js
+++ react/src/Home.js
@@ -91,11 +91,19 @@ export default class Home extends React.Component {
               </Col>
             </Row>
             <Row className='justify-content-around my-4 pb-4'>
-              <Col md='12' className='d-flex justify-content-center'>
+              <Col md={2} sm={6} className='d-flex justify-content-center'>
                 <SponsorLogo location='https://www.iiam.org/' name='IIAM' imgSrc={IIAM} size='small' />
+              </Col>
+              <Col md={2} sm={6} className='d-flex justify-content-center'>
                 <SponsorLogo location='https://ndriresource.org/' name='NDRI' imgSrc={NDRI} size='small' />
+              </Col>
+              <Col md={2} sm={6} className='d-flex justify-content-center'>
                 <SponsorLogo location='https://iidp.coh.org/' name='IIDP' imgSrc={IIDP} size='small' />
+              </Col>
+              <Col md={2} sm={6} className='d-flex justify-content-center'>
                 <SponsorLogo location='https://hirnetwork.org/' name='HIRN' imgSrc={HIRN} size='small' />
+              </Col>
+              <Col md={2} sm={6} className='d-flex justify-content-center'>
                 <SponsorLogo location='https://vumc.org' name='VUMC' imgSrc={VUMC} size='small' />
               </Col>
             </Row>
