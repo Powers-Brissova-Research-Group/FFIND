adb651ca53507f6fa21e5cf6899eab1c8f70bd22
JP Cartailler <jp.cartailler@gmail.com>
2020-08-26 18:59:51 -0500

    Spacing and content

--- react/src/components/utils/Footer.jsx
+++ react/src/components/utils/Footer.jsx
@@ -26,15 +26,29 @@ export default class Footer extends React.Component {
         <Container fluid>
           <Container>
             <Row className='v-padded'>
-              <Col md='6'>
+              <Col md='4'>
                 <p>&copy; 2020 - Copyright Notice<br/>
                   <a href="https://github.com/Powers-Brissova-Research-Group/FFIND/">Developed with FFIND</a>
                 </p>
               </Col>
 
-              <Col md='6'>
+              <Col md='4'>
+                <p>Dummy text here</p>
+                <ul>
+                  <li>List item 1</li>
+                  <li>List item 2</li>
+                  <li>List item 3</li>
+                </ul>
+              </Col>
+
+              <Col md='4'>
                 <span className='float-right scroll-button'><FontAwesomeIcon icon='hand-pointer' size='2x' id='x' onClick={this.scrollToTop} /></span>
                 <p>Dummy text here</p>
+                <ul>
+                  <li>List item 1</li>
+                  <li>List item 2</li>
+                  <li>List item 3</li>
+                </ul>
               </Col>
 
             </Row>
