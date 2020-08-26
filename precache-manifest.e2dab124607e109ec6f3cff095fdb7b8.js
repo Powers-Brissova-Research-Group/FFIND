fb871e962127fc4b90ef85f1a1f70e591fb73bd9
JP Cartailler <jp.cartailler@gmail.com>
2020-08-26 18:43:15 -0500

    Updates

---
+++ precache-manifest.e2dab124607e109ec6f3cff095fdb7b8.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8991f7771bf58b4bbb384c24900e2cf",
+    "url": "/FFIND/index.html"
+  },
+  {
+    "revision": "cfcb7e1d9c5171ac1891",
+    "url": "/FFIND/static/css/2.caeea187.chunk.css"
+  },
+  {
+    "revision": "f85b1ca69d3f8267f17f",
+    "url": "/FFIND/static/css/main.551e4026.chunk.css"
+  },
+  {
+    "revision": "cfcb7e1d9c5171ac1891",
+    "url": "/FFIND/static/js/2.e7a16f05.chunk.js"
+  },
+  {
+    "revision": "c3cd2a6642c75a89212a70a3fcd44afc",
+    "url": "/FFIND/static/js/2.e7a16f05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f85b1ca69d3f8267f17f",
+    "url": "/FFIND/static/js/main.d319dfda.chunk.js"
+  },
+  {
+    "revision": "e8060942331258f425cc",
+    "url": "/FFIND/static/js/runtime-main.e6e644f1.js"
+  },
+  {
+    "revision": "f3ce344028e40af5610ff6e8d8b27b89",
+    "url": "/FFIND/static/media/CFRD_JCI_header.f3ce3440.jpg"
+  },
+  {
+    "revision": "858bbf70848d2af906e6e494a9a29d2c",
+    "url": "/FFIND/static/media/CFRD_processing_schematic.858bbf70.jpg"
+  },
+  {
+    "revision": "038b6f0751a2e86dcca712ea20970a4a",
+    "url": "/FFIND/static/media/HANDELP_diagram.038b6f07.jpg"
+  },
+  {
+    "revision": "a58ae89b690cb7d5556a054261b3bbba",
+    "url": "/FFIND/static/media/HPAP_article.a58ae89b.jpg"
+  },
+  {
+    "revision": "5001bb5d18e779059d316406db01486e",
+    "url": "/FFIND/static/media/HPAP_schematic.5001bb5d.jpg"
+  },
+  {
+    "revision": "0b0e937cd3b5a9d9f084fea5dff9f5ce",
+    "url": "/FFIND/static/media/amyloid-formation-in-type-2-diabetes-t2d-banner.0b0e937c.jpg"
+  },
+  {
+    "revision": "8abcecbd0c3eef76714fc6bdd14b2813",
+    "url": "/FFIND/static/media/amyloid-formation-in-type-2-diabetes-t2d-banner.8abcecbd.webp"
+  },
+  {
+    "revision": "9799cec969c6003f4d6ea8a12e66c5e7",
+    "url": "/FFIND/static/media/cystic-fibrosis-related-diabetes-cfrd-banner.9799cec9.jpg"
+  },
+  {
+    "revision": "bd78764df55f53b17fb6577d8be26aa6",
+    "url": "/FFIND/static/media/cystic-fibrosis-related-diabetes-cfrd-banner.bd78764d.webp"
+  },
+  {
+    "revision": "061305d7b4e27c944bdf2189bd2874f2",
+    "url": "/FFIND/static/media/cystic-fibrosis-related-diabetes-cfrd.061305d7.jpg"
+  },
+  {
+    "revision": "a5ca11eac3f80d3329a53a7210291a22",
+    "url": "/FFIND/static/media/exocrine-pancreas-in-type-1-diabetes-banner.a5ca11ea.webp"
+  },
+  {
+    "revision": "d1cd80cb2904d71f31e9293ac643f155",
+    "url": "/FFIND/static/media/exocrine-pancreas-in-type-1-diabetes-banner.d1cd80cb.jpg"
+  },
+  {
+    "revision": "32ba2d434cf3b7a55e46030524bd4b8e",
+    "url": "/FFIND/static/media/exocrine-pancreas-in-type-1-diabetes.32ba2d43.jpg"
+  },
+  {
+    "revision": "74d6e32d59d52ddae9ceaa5004065858",
+    "url": "/FFIND/static/media/exocrine_image.74d6e32d.png"
+  },
+  {
+    "revision": "804e9a4bfac6469b19979c0a4bff659e",
+    "url": "/FFIND/static/media/handel-p-banner-old.804e9a4b.webp"
+  },
+  {
+    "revision": "ba0028764e4369748b235bc7d2664286",
+    "url": "/FFIND/static/media/handel-p-banner-old.ba002876.png"
+  },
+  {
+    "revision": "d56fa687876868557959473b5eeb5d36",
+    "url": "/FFIND/static/media/handel-p-black-full.d56fa687.png"
+  },
+  {
+    "revision": "ac9622b8212556a16162dfe1bc6af262",
+    "url": "/FFIND/static/media/handel-p-black.ac9622b8.png"
+  },
+  {
+    "revision": "edc65ab1df0ccefd6f80b26dc08b1f03",
+    "url": "/FFIND/static/media/handel-p-white-small.edc65ab1.png"
+  },
+  {
+    "revision": "4ad3e333627c5bf569fc88d043cf660d",
+    "url": "/FFIND/static/media/human-pancreas-analysis-program-hpap-banner.4ad3e333.jpg"
+  },
+  {
+    "revision": "9852cf85f30198334ea324e89067eed7",
+    "url": "/FFIND/static/media/human-pancreas-analysis-program-hpap-banner.9852cf85.webp"
+  },
+  {
+    "revision": "01f5e2ae9e0d5e67324a1a8ac8e9d194",
+    "url": "/FFIND/static/media/human-pancreas-analysis-program-hpap.01f5e2ae.webp"
+  },
+  {
+    "revision": "600f00b5c3700442d02315dca087b89b",
+    "url": "/FFIND/static/media/human-pancreas-analysis-program-hpap.600f00b5.jpg"
+  },
+  {
+    "revision": "dee7e03a522f1aa25355759c82079141",
+    "url": "/FFIND/static/media/human-pancreas-analysis-program-hpap.dee7e03a.png"
+  },
+  {
+    "revision": "24604c7216e2107cc1da64a946377de6",
+    "url": "/FFIND/static/media/human-pancreas-development-banner.24604c72.jpg"
+  },
+  {
+    "revision": "cd4b1d0c3ff1f78641c9bcc884bc2008",
+    "url": "/FFIND/static/media/human-pancreas-development-banner.cd4b1d0c.webp"
+  },
+  {
+    "revision": "20dc043e348812caec764352b6840147",
+    "url": "/FFIND/static/media/image-formats-suite-banner.20dc043e.webp"
+  },
+  {
+    "revision": "9ac1ca873bc657ff354088ebdb9ffc21",
+    "url": "/FFIND/static/media/image-formats-suite-banner.9ac1ca87.jpg"
+  },
+  {
+    "revision": "3ec3aa3382ce72cee241e8e571cc14b4",
+    "url": "/FFIND/static/media/images-with-metadata-banner.3ec3aa33.jpg"
+  },
+  {
+    "revision": "dd54add825dbaaf394d3e721a34337c9",
+    "url": "/FFIND/static/media/nPOD_article.dd54add8.png"
+  },
+  {
+    "revision": "0a12dc97204b5b939e5a8b1ae3e866e1",
+    "url": "/FFIND/static/media/neonatal-development--early-life-pancreas-handel-p-banner.0a12dc97.webp"
+  },
+  {
+    "revision": "3ec3aa3382ce72cee241e8e571cc14b4",
+    "url": "/FFIND/static/media/neonatal-development--early-life-pancreas-handel-p-banner.3ec3aa33.jpg"
+  },
+  {
+    "revision": "5d875771c0dc71ede68824173e3364d5",
+    "url": "/FFIND/static/media/neonatal-development--early-life-pancreas-handel-p.5d875771.webp"
+  },
+  {
+    "revision": "d56fa687876868557959473b5eeb5d36",
+    "url": "/FFIND/static/media/neonatal-development--early-life-pancreas-handel-p.d56fa687.png"
+  },
+  {
+    "revision": "e0b80be97dbeb49b22d16018a9cec79d",
+    "url": "/FFIND/static/media/neonatal-development--early-life-pancreas-handel-p.e0b80be9.jpg"
+  },
+  {
+    "revision": "74c46c061f8def0ae7736a75664511a7",
+    "url": "/FFIND/static/media/npod-case-6362-banner.74c46c06.jpg"
+  },
+  {
+    "revision": "86c1c51a1b4a63bfd39f6c1ad08a8206",
+    "url": "/FFIND/static/media/npod.86c1c51a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d7b4a37840b6f932de25ab7f75a12d80.js"
+  "/FFIND/precache-manifest.e2dab124607e109ec6f3cff095fdb7b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FFIND/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
